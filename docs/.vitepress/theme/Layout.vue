576dc09af37d425fda3527b15ba3b95bfe13e793
gill <e26290@gmail.com>
2025-05-17 16:31:00 +0800

    feat: 更新 Layout.vue 組件，新增 Analytics 組件並確認 onMounted 正確導入；簡化 index.md 檔案內容

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,16 +1,21 @@
 <template>
     <DefaultTheme.Layout>
         <template #aside-top>
-            <p>Layout Test</p>
+            <ClientOnly>
+                <Analytics />
+                <p>Analytics Test</p>
+            </ClientOnly>
         </template>
     </DefaultTheme.Layout>
 </template>
 
 <script setup>
-import DefaultTheme from "vitepress/theme";
-import { onMounted } from "vue";
+import DefaultTheme from 'vitepress/theme'
+import { Analytics } from '@vercel/analytics/react';
+import { ClientOnly } from 'vitepress';
+import { onMounted } from 'vue';  // 確保 onMounted 被正確導入
 
 onMounted(() => {
-    console.log("Layout.vue is mounted!");
+    console.log('Layout.vue is mounted!'); // 測試訊息
 });
 </script>
