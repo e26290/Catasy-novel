85a54f82a58d3e322a05e34aad5e5329f3a26658
gill <e26290@gmail.com>
2025-05-17 16:09:53 +0800

    feat: 新增自訂複製功能，附加版權資訊於複製內容

--- docs/.vitepress/theme/backup/custom-scripts.js
+++ docs/.vitepress/theme/backup/custom-scripts.js
@@ -3,8 +3,6 @@ if (typeof window !== 'undefined') {
     document.addEventListener('DOMContentLoaded', () => {
         document.addEventListener('contextmenu', function (event) {
             event.preventDefault();
-            // (可選) 你可以在這裡彈出一個提示訊息，但不建議，因為會很煩人
-            // alert("為了保護內容，本站已禁用右鍵選單。");
         });
         document.addEventListener('copy', function (event) {
             const selection = document.getSelection();
