885789c02851b58c6f9730c5c9286e1d847d57cb
gill <e26290@gmail.com>
2025-05-17 20:09:31 +0800

    feat: 在 index.js 中新增 Google Analytics 4 初始化邏輯，確保 onMounted 正確導入並附加 GA script

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,8 +1,29 @@
 // docs/.vitepress/theme/index.js
-import DefaultTheme from 'vitepress/theme'
-import './custom.css'
-import './custom-scripts.js'
+import DefaultTheme from 'vitepress/theme';
+import './custom.css';
+import './custom-scripts.js';
+import { onMounted } from 'vue'; // 確保 onMounted 被正確導入
+
+const GA_MEASUREMENT_ID = 'G-961CE7DB24'; // 替換成你的 GA4 Measurement ID
 
 export default {
     ...DefaultTheme,
+    setup() {
+        if (typeof window !== 'undefined') {
+            onMounted(() => {
+                window.dataLayer = window.dataLayer || [];
+                function gtag() { dataLayer.push(arguments); }
+                gtag('js', new Date());
+                gtag('config', GA_MEASUREMENT_ID);
+
+                // 創建並附加 script 標籤
+                const script = document.createElement('script');
+                script.async = true;
+                script.src = "https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}";
+                document.head.appendChild(script);
+
+                console.log('Google Analytics 4 is initialized.');
+            });
+        }
+    },
 };
