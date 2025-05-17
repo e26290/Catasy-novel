9e7dd5ec9459013b22c8cfd1ef94cbc152950ecc
gill <e26290@gmail.com>
2025-05-17 16:26:42 +0800

    feat: 移除不必要的 Analytics 和 ClientOnly 組件，簡化 Layout.vue 組件結構

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,21 +1,16 @@
 <template>
     <DefaultTheme.Layout>
-        <ClientOnly>
-            <template #aside-top>
-                <Analytics />
-                <p>Analytics Test</p>
-            </template>
-        </ClientOnly>
+        <template #aside-top>
+            <p>Layout Test</p>
+        </template>
     </DefaultTheme.Layout>
 </template>
 
 <script setup>
 import DefaultTheme from "vitepress/theme";
-import { Analytics } from "@vercel/analytics/react";
-import { ClientOnly } from "vitepress";
-import { onMounted } from "vue"; // 確保 onMounted 被正確導入
+import { onMounted } from "vue";
 
 onMounted(() => {
-    console.log("Layout.vue is mounted!"); // 測試訊息
+    console.log("Layout.vue is mounted!");
 });
 </script>
