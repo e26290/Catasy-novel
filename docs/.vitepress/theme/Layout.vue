e05ea7a588409e3b83073882f9c71b5326333e4e
gill <e26290@gmail.com>
2025-05-17 16:15:05 +0800

    feat: 新增自訂複製功能，附加版權資訊於複製內容

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -3,17 +3,19 @@
         <template #aside-top>
             <ClientOnly>
                 <Analytics />
+                <p>Analytics Test</p>
             </ClientOnly>
         </template>
     </DefaultTheme.Layout>
 </template>
 
 <script setup>
-import DefaultTheme from "vitepress/theme";
-import { Analytics } from "@vercel/analytics/react";
-import { ClientOnly } from "vitepress";
+import DefaultTheme from 'vitepress/theme'
+import { Analytics } from '@vercel/analytics/react';
+import { ClientOnly } from 'vitepress';
+import { onMounted } from 'vue';  // 確保 onMounted 被正確導入
 
-onMounted(() => {  // 在元件掛載後執行
-  console.log('Layout.vue is mounted!'); // 加入這個測試訊息
+onMounted(() => {
+    console.log('Layout.vue is mounted!'); // 測試訊息
 });
 </script>
