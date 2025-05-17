64885f72a06828c38b559453436d48d3b52bebb6
gill <e26290@gmail.com>
2025-05-17 15:59:43 +0800

    feat: 在 Layout.vue 中新增元件掛載後的測試訊息

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -13,4 +13,7 @@ import DefaultTheme from "vitepress/theme";
 import { Analytics } from "@vercel/analytics/react";
 import { ClientOnly } from "vitepress";
 
+onMounted(() => {  // 在元件掛載後執行
+  console.log('Layout.vue is mounted!'); // 加入這個測試訊息
+});
 </script>
