2392eb369594e5a685cb1c0f916cd29aa2b8b9df
gill <e26290@gmail.com>
2025-06-16 17:42:13 +0800

    feat: 更新18-19章節

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -60,6 +60,8 @@ export default {
             { text: '第十五章：心醉・夜影含情初至', link: '/novels/still-you/Ch-15' },
             { text: '第十六章：初綻・春深夜雨猶柔', link: '/novels/still-you/Ch-16' },
             { text: '第十七章：情欲・臨界心動交纏（Ｈ）', link: '/novels/still-you/Ch-17' },
+            { text: '第十八章：交融・靜夜有聲（H）', link: '/novels/still-you/Ch-18' },
+            { text: '第十九章：清晨・夢與火光之間（H）', link: '/novels/still-you/Ch-19' },
           ]
         }
       ],
